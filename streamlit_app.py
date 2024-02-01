3c233b778f76bcca87638f9156931b19104a4ec7
pborgesagir <151963659+pborgesagir@users.noreply.github.com>
2024-02-01 11:08:06 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -116,17 +116,18 @@ if authentication_status:
     
     
     col1, col2 = st.columns(2)
-    col3, col4 = st.columns(2)
-    col5, col6 = st.columns(2)
-    col7, col8, col9 = st.columns(3)
-    col10, col11 = st.columns(2)
-    col12, col13 = st.columns(2)
-    col14, col15 = st.columns(2)
-    col16 = st.columns(1)[0]
-    col17, col18 = st.columns(2)
-    col19, col20 = st.columns(2)
-    col21, col22 = st.columns(2)
-    col23, col24 = st.columns(2)
+    col3 = st.columns(1)[0]
+    col4, col5 = st.columns(2)
+    col6, col7 = st.columns(2)
+    col8, col9, col10 = st.columns(3)
+    col11, col12 = st.columns(2)
+    col13, col14 = st.columns(2)
+    col15, col16 = st.columns(2)
+    col17 = st.columns(1)[0]
+    col18, col19 = st.columns(2)
+    col20, col21 = st.columns(2)
+    col22, col23 = st.columns(2)
+    col24, col25 = st.columns(2)
 
  
     
