19609e505c6b9a2683cf819a427a2db5fa4da56f
pborgesagir <151963659+pborgesagir@users.noreply.github.com>
2024-02-01 14:14:52 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -204,7 +204,9 @@ if authentication_status:
     
     
     # Show the chart in col4 with custom width and height
-    col4.plotly_chart(fig_classificacao_diff_executado, use_container_width=True, height=800)
+    # Show the chart in col4 with custom width and height
+    col4.plotly_chart(fig_classificacao_diff_executado, width=0, height=500)
+
 
 
 
